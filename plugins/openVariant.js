d55f8fa615c40ee740074e2a95524dce0213d5a8
DaviesAmedeus <amedeusedgar@gmail.com>
2024-07-25 08:04:23 +0300

    Building the hero section

--- plugins/openVariant.js
+++ plugins/openVariant.js
@@ -3,6 +3,6 @@ const plugin = require("tailwindcss/plugin")
 const openVariant = plugin(function({addVariant}){
     addVariant("group-open", ":merge(.group).open &")
     addVariant("peer-open", ":merge(.peer).open ~ &")
-})
+});
 
 module.exports = openVariant;
